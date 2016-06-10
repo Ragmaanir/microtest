8d768396d52f42a920b51159f2f3eed72eca8a48
ragmaanir <ragmaanir@gmail.com>
2016-06-10 17:32:51 +0200

    Use compact_map

--- src/microtest/execution_context.cr
+++ src/microtest/execution_context.cr
@@ -1,6 +1,5 @@
 module Microtest
   class ExecutionContext
-
     getter results : Array(TestResult)
     getter reporters : Array(Reporter)
     getter suites : Array(Test.class)
@@ -10,7 +9,7 @@ module Microtest
     end
 
     def errors : Array(TestFailure)
-      results.map{ |res| res if res.is_a?(TestFailure) }.compact
+      results.compact_map(&.as?(TestFailure))
     end
 
     def errors?
@@ -48,8 +47,7 @@ module Microtest
     end
 
     def total_failure
-      results.count{|r| !r.success?}
+      results.count { |r| !r.success? }
     end
-
   end
 end
