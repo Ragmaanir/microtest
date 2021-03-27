fa0e680e6141a384acb8a46bf6a7a456f51c10c1
Ragmaanir <ragmaanir@gmail.com>
2021-03-27 20:30:19 +0100

    WIP

--- src/microtest/power_assert_formatter.cr
+++ src/microtest/power_assert_formatter.cr
@@ -24,37 +24,49 @@ module Microtest
         end
       end
 
-      private def grouped_lines(lines : Array(String))
+      private def grouped_lines(lines : Array(String), bar_color = BAR_COLOR)
         String.build do |io|
           if lines.size == 1
-            io << color("◆ ", BAR_COLOR)
+            io << color("◆ ", bar_color)
             io << lines.shift
             io << "\n"
           else
-            io << color("┏ ", BAR_COLOR)
+            io << color("┏ ", bar_color)
             io << lines.shift
             io << "\n"
 
             while lines.size > 1
-              io << color("┃ ", BAR_COLOR)
+              io << color("┃ ", bar_color)
               io << lines.shift
               io << "\n"
             end
 
-            io << color("┗ ", BAR_COLOR)
+            io << color("┗ ", bar_color)
             io << lines.shift
             io << "\n"
           end
         end
       end
 
+      private def simplify_value(v : V, max_length = 64) forall V
+        if v.is_a?(Array) && v.size > 1
+          String.build do |io|
+            s = 0
+            io << "["
+            io << v.map(&.inspect).take_while { |str|
+              s += str.size + 2
+              s < max_length
+            }.join(", ")
+            io << "]"
+          end
+        else
+          v.inspect
+        end
+      end
+
       private def literal(node : TerminalNode)
         v = node.value.value
-        simpler_exp = if v.is_a?(Array) && v.size > 1
-                        "[#{v[0].inspect}, ...]"
-                      else
-                        v.inspect
-                      end
+        simpler_exp = simplify_value(v)
 
         simplified = simpler_exp != node.expression
         is_complex_result = ![true, false].includes?(v)
@@ -69,24 +81,9 @@ module Microtest
         grouped_lines(lines)
       end
 
-      #
-      # When: [1].empty?
-      # => [1].empty?
-      # When: ![1].empty?
-      # =>
-      # When: !result.empty?
-      # => [elem1, ...].empty?
-      # When: result.contains?(1.minute)
-      # => [elem1, ...].contains?(00:01:00)
-      # When: nonzero?(55 * 3 + Math.sin(angle))
-      # => nonzero?(160)
       private def call_only(node : Call)
         rv = node.receiver.value.value
-        rstr = if rv.is_a?(Array) && rv.size > 1
-                 "[#{rv[0].inspect}, ...]"
-               else
-                 rv.inspect
-               end
+        rstr = simplify_value(rv)
 
         simpler_exp = String.build do |s|
           if rv
@@ -109,22 +106,17 @@ module Microtest
           end
         end
 
-        simplified = simpler_exp != node.expression
-        is_complex_result = node.value.value != false
-
         lines = [] of String
 
         lines << "#{color("assert", :red)} #{color(node.expression, :red, :bold)}"
 
-        lines << simpler_exp if simplified
+        lines << simpler_exp if simpler_exp != node.expression
 
-        lines << node.value.value.inspect if is_complex_result
+        lines << node.value.value.inspect if node.value.value != false
 
         grouped_lines(lines)
       end
 
-      # e.g.: a == b, c > d
-      #
       # When inspect-strings longer than N characters, then display compared values
       # in two lines. Otherwise display them in the same line.
       private def call_compare(node : Call)
@@ -143,22 +135,21 @@ module Microtest
           s << rval
         end
 
-        simplified = simpler_exp != node.expression
-
         lines = [] of String
 
         lines << "#{color("assert", :red)} #{color(node.expression, :red, :bold)}"
 
-        if simplified
+        if simpler_exp != node.expression
           # if the inspects of left and right dont fit in one line,
-          # display the in two lines.
-          multi_line = lval.size + rval.size > 20
+          # display them in two lines.
+          if lval.size + rval.size > 20
+            # FIXME: indicate that these two lines are not simplifications,
+            # but different values.
 
-          if multi_line
-            # FIXME: indicate that these two lines are not simplifications, but different values
-            # e.g. by highlighting the first character difference
-            lines << lval
-            lines << rval
+            diff_idx = string_diff_index(lval, rval) || raise "BUG: Strings are not different"
+
+            lines << highlight_split_char(split_string_at(lval, diff_idx))
+            lines << highlight_split_char(split_string_at(rval, diff_idx))
           else
             lines << "#{lval} #{color(node.method_name, :dark_gray)} #{rval}"
           end
@@ -167,69 +158,29 @@ module Microtest
         grouped_lines(lines)
       end
 
-      private def color(s : String, c : Symbol, m : Symbol? = nil)
-        return s if !colorize?
-        s = s.colorize(c)
-        s = s.mode(m) if m
-        s
+      private def string_diff_index(left : String, right : String) : Int32?
+        left.chars.zip?(right.chars).index { |(l, r)| l != r }
       end
 
-      # def call(node : Node) : String
-      #   # FIXME too much complex code, refactor into two methods, compact and complex or so
-      #   expressions = node.nested_expressions.uniq
-      #   complete_expression = expressions.empty? ? node : expressions.last
+      private def split_string_at(str : String, i : Int32) : Tuple(String, String, String)
+        {
+          str[0..(i - 1)],
+          str[i, 1],
+          str[(i + 1)..-1],
+        }
+      end
 
-      #   sizes = expressions.map do |ev|
-      #     {left: ev.expression.size, right: ev.value.inspect.size}
-      #   end
+      private def highlight_split_char(t : Tuple(String, String, String)) : String
+        "#{t[0]}#{color(t[1], :white, bg: :red)}#{t[2]}"
+      end
 
-      #   max_sizes = sizes.reduce({left: 0, right: 0}) do |max, s|
-      #     {
-      #       left:  [max[:left], s[:left]].max,
-      #       right: [max[:right], s[:right]].max,
-      #     }
-      #   end
-
-      #   big_bar = "=" * 50
-      #   small_bar = "-" * 50
-
-      #   is_compact = max_sizes[:left] < 32 && max_sizes[:right] < 50
-
-      #   exp_width = [8, 12, 16, 20, 24].find { |limit| max_sizes[:left] < limit } || 24
-
-      #   assert_line = [
-      #     "⚑ assert ".colorize(:red),
-      #     complete_expression.expression.to_s.colorize(:yellow),
-      #     " # ".colorize(:dark_gray),
-      #     complete_expression.value.inspect.colorize(:dark_gray),
-      #   ].join
-
-      #   expression_values = expressions[0..-2].join("\n") do |ev|
-      #     val = ev.value.inspect
-      #     exp_str = if is_compact
-      #                 "%-#{exp_width}s" % ev.expression
-      #               else
-      #                 ev.expression.to_s
-      #               end
-
-      #     [
-      #       exp_str.colorize.fore(:yellow),
-      #       (is_compact ? " => " : "\n").colorize(:light_blue),
-      #       val,
-      #       ("\n" + small_bar.colorize(:light_blue).to_s if !is_compact),
-      #     ].join.colorize(:white)
-      #   end
-
-      #   [
-      #     assert_line,
-      #     if expressions.size > 1
-      #       [
-      #         big_bar.colorize(:light_blue),
-      #         expression_values,
-      #       ].join("\n")
-      #     end,
-      #   ].join("\n")
-      # end
+      private def color(s : String, fg : Symbol, m : Symbol? = nil, bg : Symbol? = nil)
+        return s if !colorize?
+        s = s.colorize(fg)
+        s = s.mode(m) if m
+        s = s.back(bg) if bg
+        s
+      end
     end
   end
 end
