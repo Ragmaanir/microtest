bd5f5edc985423ea132e75251c625ec8966e6a04
Ragmaanir <ragmaanir@gmail.com>
2021-04-05 19:08:28 +0200

    PowerAssertFormatter: Minor adjustments

--- src/microtest/power_assert_formatter.cr
+++ src/microtest/power_assert_formatter.cr
@@ -26,20 +26,24 @@ module Microtest
         end
       end
 
-      private def grouped_lines(lines : Array(String), bar_color = BAR_COLOR) : String
-        Termart.string(colorize?) { |t| t.grouped_lines(lines) }
+      private def build_string : String
+        Termart.string(colorize?) { |t| yield t }
+      end
+
+      private def grouped_lines(lines : Array(String)) : String
+        build_string { |t| t.grouped_lines(lines, BAR_COLOR) }
       end
 
       # Returns colorized "assert x == y"
       private def formatted_assert_statement(node : CallNode | TerminalNode) : String
-        Termart.string(colorize?) { |t|
+        build_string { |t|
           t.w("assert", fg: :red)
           t.w(" ")
           t.w(node.expression, fg: :red, m: :bold)
         }
       end
 
-      private def simplify_value(v : V, max_length = 64) forall V
+      private def simplify_value(v : V, max_length = 64) : String forall V
         if v.is_a?(Array) && v.size > 1
           String.build do |io|
             s = 0
@@ -55,7 +59,7 @@ module Microtest
         end
       end
 
-      private def literal(node : TerminalNode)
+      private def literal(node : TerminalNode) : String
         v = node.value
         simpler_exp = simplify_value(v)
 
@@ -72,7 +76,7 @@ module Microtest
         grouped_lines(lines)
       end
 
-      private def call_only(node : CallNode)
+      private def call_only(node : CallNode) : String
         rv = node.receiver.value
         rstr = simplify_value(rv)
 
@@ -110,7 +114,7 @@ module Microtest
 
       # When inspect-strings longer than N characters, then display compared values
       # in two lines. Otherwise display them in the same line.
-      private def call_compare(node : CallNode)
+      private def call_compare(node : CallNode) : String
         left, right = node.receiver, node.arguments[0]
 
         lval = left.value.inspect
@@ -139,10 +143,10 @@ module Microtest
 
             diff_idx = StringUtils.diff_index(lval, rval) || raise "BUG: Strings are not different"
 
-            lines << highlight_split_char(StringUtils.split_at(lval, diff_idx))
-            lines << highlight_split_char(StringUtils.split_at(rval, diff_idx))
+            lines << highlight_split_char(lval, diff_idx)
+            lines << highlight_split_char(rval, diff_idx)
           else
-            lines << Termart.string(colorize?) { |t|
+            lines << build_string { |t|
               t.w(lval)
               t.w(" ", node.method_name, " ", fg: :dark_gray)
               t.w(rval)
@@ -153,11 +157,13 @@ module Microtest
         grouped_lines(lines)
       end
 
-      private def highlight_split_char(t : Tuple(String, String, String)) : String
-        Termart.string(colorize?) { |a|
-          a.w(t[0])
-          a.w(t[1], fg: :white, bg: :red)
-          a.w(t[2])
+      private def highlight_split_char(str : String, at : Int32) : String
+        parts = StringUtils.split_at(str, at)
+
+        build_string { |a|
+          a.w(parts[0])
+          a.w(parts[1], fg: :white, bg: :red)
+          a.w(parts[2])
         }
       end
     end
